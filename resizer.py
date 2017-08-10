fdaa669204abd4d03ab4948710a7f39cc3c4bfb5
Maria Benavente <mariabenaventeg@gmail.com>
2017-08-10 11:58:13 +0200

    first working version

--- resizer.py
+++ resizer.py
@@ -1,3 +1,19 @@
-from PIL import image
+from PIL import Image
+import os, sys, datetime
 
-# read image
+# Starting value for naming the photos
+i = 1
+groupName = sys.argv[1]
+reduction = sys.argv[2]
+nPics = sys.argv[3]
+directory = os.getcwd()
+
+for filename in os.listdir(directory):
+    if filename.lower().endswith(('.jpeg', '.jpg')):
+        img = Image.open(filename)
+        x = img.size[0]/int(reduction)
+        y = img.size[1]/int(reduction)
+        newImg = img.resize((x, y), Image.ANTIALIAS)
+        name = groupName + '_' + str(i).zfill(int(nPics))
+        newImg.save(name + ".JPEG")
+        i += 1
