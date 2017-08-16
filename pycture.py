bc0f48c1d8ada2f2afa4158a5a3d07ac342441b9
Maria Benavente <mariabenaventeg@gmail.com>
2017-08-16 13:35:54 +0200

    cleaned up and a few more parameters added

--- pycture.py
+++ pycture.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
-from PIL import Image
-from datetime import datetime
-import os, sys, getopt
+import os, sys, getopt, datetime
+from PIL import Image, ImageFile
 
 def main(argv):
     # Default parameter values
@@ -14,7 +13,7 @@ def main(argv):
 
     # Get customized parameters
     try:
-        opts, args = getopt.getopt(argv, 'hto:r:z:s:c:p:')
+        opts, args = getopt.getopt(argv, 'hto:r:z:s:c:')
     except getopt.GetoptError as err:
         # Warn about unrecognized parameters and exit
         print ("Ops... " + str(err))
@@ -22,7 +21,7 @@ def main(argv):
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print("# .====================================================.\n# |     value       |    char    |      default        |\n# |-----------------|------------|---------------------|\n# |    outputName   |     -o     |   keep filename     |\n# | reduction ratio |     -r     |         2           |\n# |     zeroFill    |     -z     |         0           |\n# |    picNumber    |     -s     |         1           |\n# |   path to sort  |     -p     |     working dir     |\n# |      timer      |     -t     |         -           |\n# |      help       |     -h     |         -           |\n# .====================================================/\n\n# Checkout the README for further information: https://github.com/mariabg/PyctureResizer")
+            print("# .====================================================.\n# |     value       |    char    |      default        |\n# |-----------------|------------|---------------------|\n# |    outputName   |     -o     |   keep filename     |\n# | reduction ratio |     -r     |         2           |\n# |     zeroFill    |     -z     |         0           |\n# |    picNumber    |     -s     |         1           |\n# |    converter    |     -c     | keep the same format|\n# |      timer      |     -t     |         -           |\n# |      help       |     -h     |         -           |\n# .====================================================/\n\n# Checkout the README for further information: https://github.com/mariabg/PyctureResizer")
             sys.exit(2)
         elif opt == '-t':
             timer = True
@@ -37,38 +36,37 @@ def main(argv):
         elif opt == '-c':
             convert = True
             cExtension = arg
-        elif opt == '-p':
-            if os.path.isdir(arg):
-                directory = arg
-            else:
-                print("Ops! You entered a wrong path")
-                sys.exit(2)
     if (timer):
-        initTime = datetime.now()
+        initTime = datetime.datetime.now()
 
-    # picture resize
     for filename in os.listdir(directory):
-        if filename.lower().endswith(('.jpeg', '.jpg', 'png', 'tif')):
+        if filename.lower().endswith(('.jpeg', '.jpg', '.png')):
             img = Image.open(filename)
             x = img.size[0]/reduction
             y = img.size[1]/reduction
             newImg = img.resize((x, y), Image.ANTIALIAS)
-            # rename or keep the filename
+
+            # Avoid problems saving larger JPEG file with PIL and the optimize flag set
+            ImageFile.MAXBLOCK = img.size[0] * img.size[1]
+
             if outputName == '':
-                # remove extension from filename
-                name = filename.split('.')[0]
-                out = name + "_" + str(picNumber).zfill(zeroFill)
+                out = filename.split('.')[0] + "_" + str(picNumber).zfill(zeroFill)
             else:
                 out = outputName + "_" + str(picNumber).zfill(zeroFill)
-            if (convert):
-                newImg.save(out + '.' +cExtension)
-            else:
-                newImg.save(out + ".JPEG")
+            if (convert == False):
+                if filename.lower().endswith('.png'):
+                    cExtension = 'png'
+                elif filename.lower().endswith('.jpg'):
+                    cExtension = 'jpg'
+                else:
+                    cExtension = 'jpeg'
+            # quality option ignored for PNG files
+            newImg.save(out + '.' + cExtension, quality=90, optimize=True)
             picNumber += 1
 
     print "Successful picture resize."
     if (timer):
-        endTime = datetime.now()
+        endTime = datetime.datetime.now()
         execTime = endTime - initTime
         print("Conversion time was " + str(execTime))
 
